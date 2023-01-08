4a6a4eb4b51db1ac00b2e54e6cba1685ee328598
andrea-maiolo <maiolo.m.2@gmail.com>
2023-01-08 11:44:55 +0100

    homepage modified

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import React from "react";
 import Nav from "./components/Nav";
+import Home from "./components/Homepage";
 
 const App = () => {
   const [cart, setCart] = useState([]);
@@ -24,6 +25,7 @@ const App = () => {
           }}
         />
       </div>
+      <Home />
     </div>
   );
 };
